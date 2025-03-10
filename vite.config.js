f1ba033e03dfc4e751ffd6c076d2e54850191280
mohamednaeemm <naemm2048@gmail.com>
2025-03-10 07:26:12 +0200

    update live version

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/contactus-react/', // 👈 Set this to your repo name
+  base: './',
 })
 
